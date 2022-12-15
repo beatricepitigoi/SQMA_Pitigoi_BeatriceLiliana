a5f05612df1722f4cb65d370d4a0b4c85ad28d9c
beatrrice <beatrice.pitigoi#@yahoo.com>
2022-12-15 23:40:43 +0200

    first commit

---
+++ src/ro/ase/ism/sqma/main/Utils.java
@@ -0,0 +1,46 @@
+package ro.ase.ism.sqma.main;
+
+public class Utils {
+
+	public boolean isPrime(int number) {
+		// Corner case
+        if (number <= 1)
+            return false;
+  
+        // Check from 2 to n-1
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0)
+                return false;
+        } 
+  
+        return true;
+		
+	}
+	
+	
+	public int sumEvenNumbers(int a, int b) {
+		int sum=0;
+		if(a % 2 == 0) {
+			sum +=a;	
+		}
+		
+		if(b % 2 == 0) {
+			sum +=b;	
+		}
+		
+		return sum;
+		
+	}
+	
+	
+	public int countLetterOccurence(String string, char letter) {
+		int count = 0;
+		int fromIndex = 0;
+		while(string.indexOf(letter, fromIndex) != -1) {
+			count ++;
+			fromIndex = string.indexOf(letter, fromIndex)+1;
+		}
+		return count;
+	}
+	
+}
---
+++ src/ro/ism/sqma/test/TestCases.java
@@ -0,0 +1,69 @@
+package ro.ism.sqma.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import ro.ase.ism.sqma.main.Utils;
+
+
+class TestCases {
+	private Utils utils;
+	
+	@BeforeEach
+	@Tag("NumberProcesses")
+	@Tag("CharacterProcesses")
+	public void setUp(){
+		utils = new Utils();
+	}
+	
+	@Test
+	@Tag("NumberProcesses")
+	public void testIsPrimeTrue() {
+		boolean result = utils.isPrime(11);
+		assertTrue(result);
+		result = utils.isPrime(23);
+		assertTrue(result);
+	}
+	
+	@Test
+	@Tag("NumberProcesses")
+	public void testIsPrimeFalse() {
+		boolean result = utils.isPrime(21);
+		assertFalse(result);
+		result = utils.isPrime(4);
+		assertFalse(result);
+		result = utils.isPrime(36);
+		assertFalse(result);
+	}
+	
+	
+	@Test
+	@Tag("NumberProcesses")
+	public void testsumEvenNumbersTrue() {
+		int result  =  utils.sumEvenNumbers(2, 2);
+		assertEquals(4, result);
+	}
+	
+	@Test
+	@Tag("CharacterProcesses")
+	public void testCountLetterOccurence1() {
+		int actual = utils.countLetterOccurence("check", 'c');
+		assertEquals(2, actual);
+	}
+
+	@Test
+	@Tag("CharacterProcesses")
+	public void testCountLetterOccurence2() {
+		int actual = utils.countLetterOccurence("Check", 'c');
+		assertEquals(1, actual);
+	}
+
+	
+
+}
